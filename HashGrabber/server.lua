86426ed11c5af14cfd49c6a542c536adb3a193f2
likizao <likidoytb@gmail.com>
2019-04-22 22:17:32 -0300

    Update server.lua

--- HashGrabber/server.lua
+++ HashGrabber/server.lua
@@ -2,11 +2,10 @@
 RegisterServerEvent('sun:hasg')
 	AddEventHandler('sun:hasg', function(hash,Model)
 	
-      SendWebhookMessage(ac_webhook_hash, "**Nome Veiculo** \n```\nHash: "..hash.." Modelo: "..Model.."```")
+      SendWebhookMessage("webhook", "{modelHash = "..hash..", modelName = '"..Model.."'")
 end)
 
 Citizen.CreateThread(function()
-	ac_webhook_hash = GetConvar("ac_webhook_hash", "none")
 	function SendWebhookMessage(webhook,message)
 		if webhook ~= "none" then
 			PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({content = message}), { ['Content-Type'] = 'application/json' })
