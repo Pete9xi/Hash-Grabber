77b7792f13bf3073bba2eca792ffb15a7d142a09
Likizao <likidoytb@gmail.com>
2019-06-22 02:08:36 -0300

    Formato próprio para LSC e melhorias em geral.

--- HashGrabber/server.lua
+++ HashGrabber/server.lua
@@ -1,14 +1,32 @@
 
-RegisterServerEvent('sun:hasg')
-	AddEventHandler('sun:hasg', function(hash,Model)
-	
-      SendWebhookMessage("webhook", "{modelHash = "..hash..", modelName = '"..Model.."'")
+local webhookurl = 'https://discordapp.com/api/webhooks/XXXXXXXXX/XXXCHANGEMEXXX' -- Insira seu Webhook dentro dos apóstrofos.
+local modeloparaLSC = false; -- Se você precisa que gere num formato semelhante ao necessário para colocar na Los Santos, deixe true. 
+
+
+RegisterServerEvent('likzao:hashenvio')
+AddEventHandler('likzao:hashenvio', function(hash,Model)
+      if modeloparaLSC then 
+	      SendWebhookMessage("{modelHash = "..hash..", modelName = '"..Model.."'")      
+      else
+          local fields = {}
+          table.insert(fields, { name = "Veículo:", value = Model, inline = true })
+          table.insert(fields, { name = "Hash:", value = hash, inline = true })
+          PerformHttpRequest(webhookurl, function(err, text, headers) end, 'POST', json.encode(
+                    {
+                        username = "Hash Grabber",
+                        content = "*github.com/likizao/Hash-Grabber/*",
+                        embeds = {
+                            {
+                                color = 16769280,
+                                fields = fields
+                            }
+                        }
+                    }), { ['Content-Type'] = 'application/json' })
+     end
 end)
 
-Citizen.CreateThread(function()
-	function SendWebhookMessage(webhook,message)
-		if webhook ~= "none" then
-			PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({content = message}), { ['Content-Type'] = 'application/json' })
+function SendWebhookMessage(message)
+		if webhookurl ~= nil then
+			PerformHttpRequest(webhookurl, function(err, text, headers) end, 'POST', json.encode({content = message}), { ['Content-Type'] = 'application/json' })
 		end
-	end
-end)
+end	
