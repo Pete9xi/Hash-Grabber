5d2b0daa4122ce2468f7150fb7dfa2e6f5f0293f
Likizao <likidoytb@gmail.com>
2019-12-02 16:33:58 -0300

    Update server.lua

--- HashGrabber/server.lua
+++ HashGrabber/server.lua
@@ -1,12 +1,12 @@
 
-local webhookurl = 'https://discordapp.com/api/webhooks/XXXXXXXXX/XXXCHANGEMEXXX' -- Insira seu Webhook dentro dos apóstrofos.
-local modeloparaLSC = false; -- Se você precisa que gere num formato semelhante ao necessário para colocar na Los Santos, deixe true. 
+local webhookurl = 'https://discordapp.com/api/webhooks/651143701682192395/i3CvDgXEYv5JJchGvcqPKFPlU6SOOLF4FFP0MKuJEW7w2aRAQM2vvi7XOJzaLc235x-a' -- Insira seu Webhook dentro dos apóstrofos.
+local modeloparaLSC = true; -- Se você precisa que gere num formato semelhante ao necessário para colocar na Los Santos, deixe true. 
 
 
 RegisterServerEvent('likzao:hashenvio')
 AddEventHandler('likzao:hashenvio', function(hash,Model)
-      if modeloparaLSC then 
-	      SendWebhookMessage("{modelHash = "..hash..", modelName = '"..Model.."'")      
+      if modeloparaLSC then 	
+	      SendWebhookMessage("{hash = "..hash..", name = '"..Model.."', price = 0, banido = false")      
       else
           local fields = {}
           table.insert(fields, { name = "Veículo:", value = Model, inline = true })
